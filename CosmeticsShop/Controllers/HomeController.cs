dfd80f4bc170009c531f514bb6d5c46f8b329460
RedmiBook <RedmiBook@KROLIK>
2024-09-23 14:41:25 +0300

    Добавьте файлы проекта.

---
+++ CosmeticsShop/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using CosmeticsShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CosmeticsShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
